e7f1de3560105e902bab6f9da640d1739da6c91d
alvindo <alvindo56@gmail.com>
2024-02-27 21:12:51 +0700

    initial commit

---
+++ src/main/java/com/enigma/wmbapinext/WmbApiNextApplication.java
@@ -0,0 +1,13 @@
+package com.enigma.wmbapinext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WmbApiNextApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WmbApiNextApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enigma/wmbapinext/WmbApiNextApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enigma.wmbapinext;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WmbApiNextApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

2105c44f75e7068a829731ed429dd6944f1552a8
alvindo <alvindo56@gmail.com>
2024-03-01 05:19:43 +0700

    [FEATURE] add entity Customer :tada:

---
+++ src/main/java/com/enigma/wmbapinext/constant/TableName.java
@@ -0,0 +1,8 @@
+package com.enigma.wmbapinext.constant;
+
+public class TableName {
+    public static final String M_CUSTOMER = "m_customer";
+    public static final String M_TABLE = "m_table";
+    public static final String M_TRANS_TYPE = "m_trans_type";
+    public static final String M_MMENU = "m_menu";
+}
---
+++ src/main/java/com/enigma/wmbapinext/entity/Customer.java
@@ -0,0 +1,24 @@
+package com.enigma.wmbapinext.entity;
+
+import com.enigma.wmbapinext.constant.TableName;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = TableName.M_CUSTOMER)
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "phone_number")
+    private String phone;
+}
